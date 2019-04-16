4e74de5b54fce9df422be1bf3599de5ac4a74876
vanya andreychuk <vandreychyk@gmail.com>
2019-04-16 18:43:21 +0300

    - add SignOnUA

--- read_data.go
+++ read_data.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/andreychuk/num2word"
 	"io/ioutil"
+	"strconv"
 	"time"
 )
 
@@ -31,4 +32,39 @@ func (data *Data) setSignOn(str string) {
 	} else {
 		data.SignOn, _ = time.Parse("2006-01-02", str)
 	}
+	data.setSingOnUA()
+}
+
+func (data *Data) setSingOnUA() {
+	result := strconv.Itoa(data.SignOn.Day()) + " "
+	m := data.SignOn.Month()
+	switch m {
+	case time.Month(time.January):
+		result += "січня "
+	case time.Month(time.February):
+		result += "лютого "
+	case time.Month(time.March):
+		result += "березня "
+	case time.Month(time.April):
+		result += "квітня "
+	case time.Month(time.May):
+		result += "травня "
+	case time.Month(time.June):
+		result += "червня "
+	case time.Month(time.July):
+		result += "липня "
+	case time.Month(time.August):
+		result += "серпня "
+	case time.Month(time.September):
+		result += "вересня "
+	case time.Month(time.October):
+		result += "жовтня "
+	case time.Month(time.November):
+		result += "листопада "
+	case time.Month(time.December):
+		result += "грудня "
+	}
+	result += strconv.Itoa(data.SignOn.Year()) + " р."
+
+	data.SignOnUA = result
 }
